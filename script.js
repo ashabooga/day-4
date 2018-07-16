00a7467aeb1c4c54fc27e43c8944e09da70a2d6c
Benjamin Cuello-Wolffe <student@Benjamin-Cuello-Wolffe.local>
2018-07-16 11:02:25 -0400

    got rid of text

--- script.js
+++ script.js
@@ -24,27 +24,18 @@ function moveRock(e) {
    rock.style.left = "350px";
    paper.style.display = "none";
    scissors.style.display = "none";
-   vs.style.display = "initial";
-   you.style.display = "initial";
-   cpu.style.display = "initial";
 }
 
 function movePaper(e) {
    paper.style.left = "350px";
    rock.style.display = "none";
    scissors.style.display = "none";
-   vs.style.display = "initial";
-   you.style.display = "initial";
-   cpu.style.display = "initial";
 }
 
 function moveScissors(e) {
    scissors.style.left = "350px";
    rock.style.display = "none";
    paper.style.display = "none";
-   vs.style.display = "initial";
-   you.style.display = "initial";
-   cpu.style.display = "initial";
 }
 
 cpuChoice = Math.floor((Math.random() * 3) + 1);
@@ -53,13 +44,10 @@ document.querySelector('.rock').addEventListener('click', function() {
 	userChoice = 1;
 	if(cpuChoice === 1){
 		enemy_rock.style.display = "initial";
-		youTie.style.display = "initial";
 	} else if(cpuChoice === 2){
 		enemy_paper.style.display = "initial";
-		youLose.style.display = "initial";
 	} else if(cpuChoice === 3){
 		enemy_scissors.style.display = "initial";
-		youWin.style.display = "initial";
 	}
 });
 
@@ -67,13 +55,11 @@ document.querySelector('.paper').addEventListener('click', function() {
 	userChoice = 2;
 	if(cpuChoice === 1){
 		enemy_rock.style.display = "initial";
-		youWin.style.display = "initial";
 	} else if(cpuChoice === 2){
 		enemy_paper.style.display = "initial";
-		youTie.style.display = "initial";
 	} else if(cpuChoice === 3){
 		enemy_scissors.style.display = "initial";
-		youLose.style.display = "initial";
+
 	}
 });
 
@@ -81,12 +67,9 @@ document.querySelector('.scissors').addEventListener('click', function() {
 	userChoice = 3;
 	if(cpuChoice === 1){
 		enemy_rock.style.display = "initial";
-		youLose.style.display = "initial";
 	} else if(cpuChoice === 2){
 		enemy_paper.style.display = "initial";
-		youWin.style.display = "initial";
 	} else if(cpuChoice === 3){
 		enemy_scissors.style.display = "initial";
-		youTie.style.display = "initial";
 	}
 });
